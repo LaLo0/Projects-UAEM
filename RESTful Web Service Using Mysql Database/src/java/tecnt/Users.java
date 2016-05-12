651b87f29522c0bee86e7291f8f21e27e205c8b5
Alexis Ayala Solorio <lex.solorio15@gmail.com>
2016-05-12 14:05:31 -0500

    RESTful Web Service Using MySQL Database

---
+++ RESTful Web Service Using Mysql Database/src/java/tecnt/Users.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tecnt;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author alexis
+ */
+@Entity
+@Table(name = "users")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Users.findAll", query = "SELECT u FROM Users u"),
+    @NamedQuery(name = "Users.findById", query = "SELECT u FROM Users u WHERE u.id = :id"),
+    @NamedQuery(name = "Users.findByNombreEmp", query = "SELECT u FROM Users u WHERE u.nombreEmp = :nombreEmp"),
+    @NamedQuery(name = "Users.findByApellidoEmp", query = "SELECT u FROM Users u WHERE u.apellidoEmp = :apellidoEmp"),
+    @NamedQuery(name = "Users.findByDireccionEmp", query = "SELECT u FROM Users u WHERE u.direccionEmp = :direccionEmp")})
+public class Users implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "nombre_emp")
+    private String nombreEmp;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "apellido_emp")
+    private String apellidoEmp;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "direccion_emp")
+    private String direccionEmp;
+
+    public Users() {
+    }
+
+    public Users(Integer id) {
+        this.id = id;
+    }
+
+    public Users(Integer id, String nombreEmp, String apellidoEmp, String direccionEmp) {
+        this.id = id;
+        this.nombreEmp = nombreEmp;
+        this.apellidoEmp = apellidoEmp;
+        this.direccionEmp = direccionEmp;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombreEmp() {
+        return nombreEmp;
+    }
+
+    public void setNombreEmp(String nombreEmp) {
+        this.nombreEmp = nombreEmp;
+    }
+
+    public String getApellidoEmp() {
+        return apellidoEmp;
+    }
+
+    public void setApellidoEmp(String apellidoEmp) {
+        this.apellidoEmp = apellidoEmp;
+    }
+
+    public String getDireccionEmp() {
+        return direccionEmp;
+    }
+
+    public void setDireccionEmp(String direccionEmp) {
+        this.direccionEmp = direccionEmp;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Users)) {
+            return false;
+        }
+        Users other = (Users) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "tecnt.Users[ id=" + id + " ]";
+    }
+    
+}
