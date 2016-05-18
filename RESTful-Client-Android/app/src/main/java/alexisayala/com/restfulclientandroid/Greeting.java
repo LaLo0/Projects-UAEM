4ade548b77f665558b5cd9b5fc07780b763e0eea
Alexis Ayala Solorio <lex.solorio15@gmail.com>
2016-05-18 12:07:05 -0500

    RESTful Client Android

---
+++ RESTful-Client-Android/app/src/main/java/alexisayala/com/restfulclientandroid/Greeting.java
@@ -0,0 +1,17 @@
+package alexisayala.com.restfulclientandroid;
+
+/**
+ * Created by alexis on 18/05/16.
+ */
+public class Greeting {
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
