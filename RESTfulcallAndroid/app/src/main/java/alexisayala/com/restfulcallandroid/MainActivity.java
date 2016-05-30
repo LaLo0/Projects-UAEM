b3e45d0e9b4f252e03cb6eb4831b2c1a243e0725
Alexis Ayala Solorio <lex.solorio15@gmail.com>
2016-05-30 12:52:12 -0500

    Get RestFul Client pokeapi.co Funcional
    recibe el json remotamente

--- RESTfulcallAndroid/app/src/main/java/alexisayala/com/restfulcallandroid/MainActivity.java
+++ RESTfulcallAndroid/app/src/main/java/alexisayala/com/restfulcallandroid/MainActivity.java
@@ -1,52 +1,33 @@
 package alexisayala.com.restfulcallandroid;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private EditText inputData;
+    private Button buscardato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        inputData = (EditText) findViewById(R.id.inputdato);
+        buscardato = (Button) findViewById(R.id.buscar);
     }
+    public void buscardato(View view) {
+        String titulo = inputData.getText().toString();
+        if (!TextUtils.isEmpty(titulo)) {
+            String url = String.format(
+                    "http://mymovieapi.com/?title=%1$s&type=json&limit=10",
+                    titulo);
+            new LoadFilmTask().execute(url);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
